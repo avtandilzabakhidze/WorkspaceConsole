5664d45c66eede86904335ca11cbc1537f5c7b09
avtandil zabakhidze <zabakhidze01@gmail.com>
2024-02-24 15:35:56 +0400

    Initial project setup

---
+++ src/main/java/com/example/workspaceconsole/WorkspaceConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.example.workspaceconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkspaceConsoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workspaceconsole/WorkspaceConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workspaceconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceConsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
