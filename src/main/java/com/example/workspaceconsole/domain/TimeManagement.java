f3a69e2d5821aeffec8b65731b11004babccdefe
avtandil zabakhidze <zabakhidze01@gmail.com>
2024-02-27 03:26:10 +0400

    added new field to TimeManagement Enum

--- src/main/java/com/example/workspaceconsole/domain/TimeManagement.java
+++ src/main/java/com/example/workspaceconsole/domain/TimeManagement.java
@@ -1,8 +1,24 @@
 package com.example.workspaceconsole.domain;
 
 public enum TimeManagement {
-    HOUR,
-    DAY,
-    MONTH,
-    YEAR;
+    HOUR("Hour", 1),
+    DAY("Day", 24),
+    MONTH("Month", 720), // 30 days per month like mobile provider
+    YEAR("Year", 8760); //  365 days per year
+
+    private final String label;
+    private final int hoursInUnit;
+
+    TimeManagement(String label, int hoursInUnit) {
+        this.label = label;
+        this.hoursInUnit = hoursInUnit;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public int getHoursInUnit() {
+        return hoursInUnit;
+    }
 }
