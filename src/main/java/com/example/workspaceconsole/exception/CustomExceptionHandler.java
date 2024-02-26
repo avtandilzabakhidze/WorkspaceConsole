093cec0777fd55e0a810928723915af8c6ed2fd7
avtandil zabakhidze <zabakhidze01@gmail.com>
2024-02-26 21:59:57 +0400

    Added exception handler

--- src/main/java/com/example/workspaceconsole/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/workspaceconsole/exception/CustomExceptionHandler.java
@@ -3,17 +3,26 @@ package com.example.workspaceconsole.exception;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class CustomExceptionHandler {
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
-        String errorMessage = "Entity not found";
-        HttpStatus status = HttpStatus.NOT_FOUND;
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException validException, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        validException.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 
-        return new ResponseEntity<>(errorMessage, status);
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException entityNotFoundException, WebRequest request) {
+        String errorMessage = "Entity not found";
+        return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
     }
 }
